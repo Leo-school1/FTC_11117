82cd2acb04749576c25795f27b53065345de496c
Leo <28leom@students.spa.edu>
2025-10-14 16:05:52 -0500

    fixed autonomous with DcMotorEx for setVelocity()

--- Auto.java
+++ Auto.java
@@ -1,29 +1,83 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-
-//@Disabled
 @Autonomous
 
-public class Autonomous extends OpMode {
+public class Auto extends OpMode {
+    
+    final int VELOCITY = 100;
+
+    private DcMotorEx leftFrontDrive = null;
+    private DcMotorEx rightFrontDrive = null;
+    private DcMotorEx leftBackDrive = null;
+    private DcMotorEx rightBackDrive = null;
+    private DcMotorEx launcher = null;
+    private CRServo leftFeeder = null;
+    private CRServo rightFeeder = null;
+    
     @Override
     public void init() {
-
+        telemetry.addData("Status", "Initialized");
+        
+        leftFrontDrive = hardwareMap.get(DcMotorEx.class, "left_front_drive");
+        rightFrontDrive = hardwareMap.get(DcMotorEx.class, "right_front_drive");
+        leftBackDrive = hardwareMap.get(DcMotorEx.class, "left_back_drive");
+        rightBackDrive = hardwareMap.get(DcMotorEx.class, "right_back_drive");
+        launcher = hardwareMap.get(DcMotorEx.class, "launcher");
+        leftFeeder = hardwareMap.get(CRServo.class, "left_feeder");
+        rightFeeder = hardwareMap.get(CRServo.class, "right_feeder");
+        
     }
+
 
     @Override
     public void loop() {
-
+        telemetry.addData("Looping", "a");
+    
+        telemetry.update();
+        
+        forward(200);
+    }
+    
+    public void forward(int ticks) {
+        leftFrontDrive.setTargetPosition(ticks);
+        rightFrontDrive.setTargetPosition(ticks);
+        leftBackDrive.setTargetPosition(ticks);
+        rightBackDrive.setTargetPosition(ticks);
+        
+        leftFrontDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightFrontDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        leftBackDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightBackDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        
+        leftFrontDrive.setVelocity(VELOCITY);
+        rightFrontDrive.setVelocity(VELOCITY);
+        leftBackDrive.setVelocity(VELOCITY);
+        rightBackDrive.setVelocity(VELOCITY);
+        
+        leftFrontDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightFrontDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftBackDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightBackDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
     }
 }
+
+
+
+
+
+
+
