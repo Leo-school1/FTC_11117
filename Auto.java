8731062cc01fe79394014e3daa427630b018e4d6
Leo <28leom@students.spa.edu>
2025-11-19 09:44:14 -0600

    fixed autonomous finally

--- Auto.java
+++ Auto.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
@@ -16,90 +17,114 @@ import java.util.Date;
 
 @Autonomous
 
-public class Auto extends OpMode {
+public class Auto extends LinearOpMode {
     
-    final int VELOCITY = 100;
+    final int VELOCITY = 300;
 
     private DcMotorEx leftFrontDrive = null;
     private DcMotorEx rightFrontDrive = null;
     private DcMotorEx leftBackDrive = null;
     private DcMotorEx rightBackDrive = null;
-    private DcMotorEx launcher = null;
+    private DcMotorEx rightLauncher = null;
+    private DcMotorEx leftLauncher = null;
+    private DcMotorEx intakeMotor = null;
+    private DcMotorEx elevator = null;
     private CRServo leftFeeder = null;
     private CRServo rightFeeder = null;
-    
+
     @Override
-    public void init() {
-        telemetry.addData("Status", "Initialized");
-        
+    public void runOpMode() {
+                
         leftFrontDrive = hardwareMap.get(DcMotorEx.class, "left_front_drive");
         rightFrontDrive = hardwareMap.get(DcMotorEx.class, "right_front_drive");
         leftBackDrive = hardwareMap.get(DcMotorEx.class, "left_back_drive");
         rightBackDrive = hardwareMap.get(DcMotorEx.class, "right_back_drive");
-        launcher = hardwareMap.get(DcMotorEx.class, "launcher");
+        rightLauncher = hardwareMap.get(DcMotorEx.class, "right_launcher");
+        leftLauncher = hardwareMap.get(DcMotorEx.class, "left_launcher");
+        intakeMotor = hardwareMap.get(DcMotorEx.class, "intake_motor");
+        elevator = hardwareMap.get(DcMotorEx.class, "elevator_motor");
         leftFeeder = hardwareMap.get(CRServo.class, "left_feeder");
         rightFeeder = hardwareMap.get(CRServo.class, "right_feeder");
         
-    }
+        leftFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+        leftBackDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightBackDrive.setDirection(DcMotor.Direction.REVERSE);
+        leftLauncher.setDirection(DcMotor.Direction.REVERSE);
+        elevator.setDirection(DcMotor.Direction.FORWARD);
 
+        leftFrontDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightFrontDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftBackDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightBackDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-    @Override
-    public void loop() {
-        telemetry.addData("Looping", "a");
-    
+        // sets the coefficients to run
+        //setPositionCoefficient();
+
+        telemetry.addData("Status", "Initialized");
         telemetry.update();
+
+        waitForStart();
+
+        forward(500);
+
+    }
+    
+    public void runWheels(int leftFront, int rightFront, int leftBack, int rightBack) {
+
+        leftFrontDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightFrontDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftBackDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightBackDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        leftFrontDrive.setTargetPosition(leftFront);
+        rightFrontDrive.setTargetPosition(rightFront);
+        leftBackDrive.setTargetPosition(leftBack);
+        rightBackDrive.setTargetPosition(rightBack);
         
-        forward(200);
+        leftFrontDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightFrontDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        leftBackDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightBackDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        
+        leftFrontDrive.setPower(1.0);
+        rightFrontDrive.setPower(1.0);
+        leftBackDrive.setPower(1.0);
+        rightBackDrive.setPower(1.0);
+
+        while (leftFrontDrive.isBusy() || rightFrontDrive.isBusy() || 
+        leftBackDrive.isBusy() || rightBackDrive.isBusy()) {
+            telemetry.addData("Left F: ", leftFrontDrive.getCurrentPosition());
+            telemetry.addData("Right F: ", rightFrontDrive.getCurrentPosition());
+            telemetry.addData("Left B: ", leftBackDrive.getCurrentPosition());
+            telemetry.addData("Rigth B: ", rightBackDrive.getCurrentPosition());
+            telemetry.update();
+        } // waits until they are done
+
+        
+        leftFrontDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightFrontDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftBackDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightBackDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        
+        leftFrontDrive.setPower(0);
+        rightFrontDrive.setPower(0);
+        leftBackDrive.setPower(0);
+        rightBackDrive.setPower(0);
+        //sleep(100); 
     }
     
     public void forward(int ticks) {
-        leftFrontDrive.setTargetPosition(ticks);
-        rightFrontDrive.setTargetPosition(ticks);
-        leftBackDrive.setTargetPosition(ticks);
-        rightBackDrive.setTargetPosition(ticks);
-        
-        leftFrontDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        rightFrontDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        leftBackDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        rightBackDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        
-        leftFrontDrive.setVelocity(VELOCITY);
-        rightFrontDrive.setVelocity(VELOCITY);
-        leftBackDrive.setVelocity(VELOCITY);
-        rightBackDrive.setVelocity(VELOCITY);
-        
-        leftFrontDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rightFrontDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        leftBackDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rightBackDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        telemetry.addData("ticks", ticks);
+        telemetry.update();
+        runWheels(ticks, ticks, ticks, ticks);
     }
-    
+
     public void right(int ticks) {
-        leftFrontDrive.setTargetPosition(ticks);
-        rightFrontDrive.setTargetPosition(ticks);
-        leftBackDrive.setTargetPosition(ticks);
-        rightBackDrive.setTargetPosition(ticks);
-        
-        leftFrontDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        rightFrontDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        leftBackDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        rightBackDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        
-        leftFrontDrive.setVelocity(VELOCITY);
-        rightFrontDrive.setVelocity(VELOCITY);
-        leftBackDrive.setVelocity(VELOCITY);
-        rightBackDrive.setVelocity(VELOCITY);
-        
-        leftFrontDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rightFrontDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        leftBackDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rightBackDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        runWheels(ticks, -ticks, -ticks, ticks);
+    }
+
+    public void rotate(int ticks) {
+        runWheels(ticks, -ticks, ticks, -ticks);
     }
 }
-
-
-
-
-
-
-
