52bd166ed7f44f2e4bcd78fa2d5373081f5a79a7
Leo <28leom@students.spa.edu>
2025-12-04 08:46:25 -0600

    changed auto.java

--- Auto.java
+++ Auto.java
@@ -1,6 +1,12 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+//import org.firstinspires.ftc.teamcode.aprilTag;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.CRServo;
@@ -14,12 +20,11 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 @Autonomous
 
 public class Auto extends LinearOpMode {
-    
-    final int VELOCITY = 300;
 
     private DcMotorEx leftFrontDrive = null;
     private DcMotorEx rightFrontDrive = null;
@@ -31,6 +36,12 @@ public class Auto extends LinearOpMode {
     private DcMotorEx elevator = null;
     private CRServo leftFeeder = null;
     private CRServo rightFeeder = null;
+
+    private AprilTagProcessor tag;
+    private VisionPortal visionPortal;
+    private List<AprilTagDetection> detections = tag.getDetections();
+
+    private ElapsedTime feedTimer = new ElapsedTime();
 
     @Override
     public void runOpMode() {
@@ -66,7 +77,9 @@ public class Auto extends LinearOpMode {
 
         waitForStart();
 
-        forward(500);
+        forward(1000);
+        right(1000);
+        shoot(3);
 
     }
     
@@ -87,10 +100,10 @@ public class Auto extends LinearOpMode {
         leftBackDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         rightBackDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         
-        leftFrontDrive.setPower(1.0);
-        rightFrontDrive.setPower(1.0);
-        leftBackDrive.setPower(1.0);
-        rightBackDrive.setPower(1.0);
+        leftFrontDrive.setPower(0.8);
+        rightFrontDrive.setPower(0.8);
+        leftBackDrive.setPower(0.8);
+        rightBackDrive.setPower(0.8);
 
         while (leftFrontDrive.isBusy() || rightFrontDrive.isBusy() || 
         leftBackDrive.isBusy() || rightBackDrive.isBusy()) {
@@ -114,17 +127,96 @@ public class Auto extends LinearOpMode {
         //sleep(100); 
     }
     
-    public void forward(int ticks) {
-        telemetry.addData("ticks", ticks);
-        telemetry.update();
+    public void forward(double mm) {
+        int ticks = (int)((mm * 537.6) / (96 * Math.PI));
         runWheels(ticks, ticks, ticks, ticks);
     }
 
-    public void right(int ticks) {
+    public void right(double mm) {
+        int ticks = (int)((mm * 537.6) / (96 * Math.PI));
         runWheels(ticks, -ticks, -ticks, ticks);
     }
 
-    public void rotate(int ticks) {
+    public void rotate(double mm) {
+        int ticks = (int)((mm * 537.6) / (96 * Math.PI));
         runWheels(ticks, -ticks, ticks, -ticks);
     }
+
+    public void shoot(int shots) {
+        rightLauncher.setVelocity(1010);
+        leftLauncher.setVelocity(1010);
+
+        while (rightLauncher.getVelocity() < 1000
+            && leftLauncher.getVelocity() < 1000) {}
+
+        intakeMotor.setPower(1.0);
+        elevator.setPower(1.0);
+        rightFeeder.setPower(1.0);
+        leftFeeder.setPower(-1.0);
+
+        feedTimer.reset();
+        while (feedTimer.seconds() < 0.1 + (0.2 * shots)) {}
+
+        rightLauncher.setVelocity(0);
+        leftLauncher.setVelocity(0);
+        intakeMotor.setPower(0.0);
+        elevator.setPower(0.0);
+        rightFeeder.setPower(0.0);
+        leftFeeder.setPower(0.0);
+    
+    }
+    public void aprilTagInit() {
+        // Create the AprilTag processor.
+        tag = new AprilTagProcessor.Builder().build();
+
+        VisionPortal.Builder builder = new VisionPortal.Builder();
+
+        
+        builder.setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"));
+
+        // IDK IF THIS IS NECESSARY BUT IT DOESN'T BUILD WITH THIS
+        //builder.setCameraResolution(new Size(640, 480));
+
+        builder.addProcessor(tag);
+
+        visionPortal = builder.build();
+
+        // Disable or re-enable the aprilTag processor at any time.
+        //visionPortal.setProcessorEnabled(tag, true);
+    }
+    public void localize() {
+        detections = tag.getDetections();
+        telemetry.addData("# AprilTags Detected", detections.size());
+
+        for (AprilTagDetection detection : detections) {
+            if (detection.metadata == null) {
+                telemetry.addLine("No Metadata");
+            } else {
+                telemetry.addLine(String.format("ID: ", detection.id, "name: ", detection.metadata.name));
+
+                telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch?)",
+                        detection.robotPose.getPosition().x,
+                        detection.robotPose.getPosition().y,
+                        detection.robotPose.getPosition().z));
+                telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg?)",
+                        detection.robotPose.getOrientation().getPitch(AngleUnit.DEGREES),
+                        detection.robotPose.getOrientation().getRoll(AngleUnit.DEGREES),
+                        detection.robotPose.getOrientation().getYaw(AngleUnit.DEGREES)));
+            }
+
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
